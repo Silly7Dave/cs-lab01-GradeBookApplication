c04d6adf39c2dcdf9c2cda4717c54e4b814c7a23
Dawid Żarnecki <dawid.zarnecki@microsoft.wsei.edu.pl>
2023-03-16 00:18:20 +0100

    - Update BaseGradeBook.GetGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -10,15 +10,17 @@ using System.Runtime.CompilerServices;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
+        public bool IsWeighted { get; set;}
         public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,18 +109,21 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            switch (letterGrade)
+            var bonus = 0;
+            if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled)
+                bonus++;
+                switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + bonus;
                 case 'B':
-                    return 3;
+                    return 3 + bonus;
                 case 'C':
-                    return 2;
+                    return 2 + bonus;
                 case 'D':
-                    return 1;
+                    return 1 + bonus;
                 case 'F':
-                    return 0;
+                    return 0 + bonus;
             }
             return 0;
         }
