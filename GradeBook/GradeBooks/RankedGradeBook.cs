c07886006fab670699d179269aacf2b64c2e4c97
Dawid Żarnecki <dawid.zarnecki@microsoft.wsei.edu.pl>
2023-03-15 23:05:17 +0100

    - update StartingUserInterfaces's HelpCommand method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -11,18 +12,32 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-        public virtual char GetLetterGrade(double averageGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
-            if (averageGrade >= 90)
-                return 'A';
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Less than 5 students to calculate");
             else if (averageGrade >= 80)
-                return 'B';
-            else if (averageGrade >= 70)
-                return 'C';
+                return 'A';
             else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
                 return 'D';
             else
                 return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else base.CalculateStudentStatistics(name);
         }
     }
 }
